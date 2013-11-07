39be7e37c85903d29fe91809951def65e849dd7f
doun <doun@163.com>
2013-11-07 10:13:08 -0500

    Init, created with rails -T option

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Car4home::Application.config.secret_key_base = '52bcf1b1206da019f1b215b46b05d21be661bb58c357b3efdbfd40926865a05686649b1877b7a77cfdcf516d29ca8d6febde617d7a50e0d9514729e14d33845a'
